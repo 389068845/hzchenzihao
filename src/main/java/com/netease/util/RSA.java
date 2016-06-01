af7712e06059f66802e9b283c9500ba125a4f0f2
hzchenzihao <hzchenzihao@HIH-D-8205.hz.ntes.domain>
2016-06-01 14:55:57 +0800

    as

--- src/main/java/com/netease/util/RSA.java
+++ src/main/java/com/netease/util/RSA.java
@@ -1,10 +1,16 @@
 package com.netease.util;
 import java.security.Key;
 import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
-import org.apache.commons.codec.binary.Base64;
+
 import javax.crypto.Cipher;
+
+import org.apache.commons.codec.binary.Base64;
 
 /*
  * FileName: RSA.java
@@ -26,15 +32,150 @@ import javax.crypto.Cipher;
  */
 public class RSA
 {
-	public static void main(String[] args)
-	{
-		String token = new RSA().encode(
-				"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDCNK/aN0bWweKdu1qw0J7TCUke6kEnvzBYO8Vr8UtFXO6rSWMA6v5y53wF1t6lSyzr549jE/Rp7LrWJ6X6HCrYHOJoecEzUojbAWEtApSc7jSkZHfgfMOx4Z8zRCM/JmvoiE9HlOAnGlxt4GDCDPHKULpVl0BmHx8KJl5ClKV2wQIDAQAB",
-				"1234");
-		System.out.println(token);
-		System.out.println(org.apache.commons.codec.binary.Base64.decodeBase64("1234"));
-	}
+	
+	
+	   public static RSAPrivateKey loadPrivateKeyByStr(String privateKeyStr)  
+	            throws Exception {  
+	        try {  
+	            byte[] buffer = org.apache.commons.codec.binary.Base64.decodeBase64(privateKeyStr);  
+	            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(buffer);  
+	            KeyFactory keyFactory = KeyFactory.getInstance("RSA");  
+	            return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);  
+	        } catch (NoSuchAlgorithmException e) {  
+	            throw new Exception("无此算法");  
+	        } catch (InvalidKeySpecException e) {  
+	            throw new Exception("私钥非法");  
+	        } catch (NullPointerException e) {  
+	            throw new Exception("私钥数据为空");  
+	        }  
+	    } 
+	   
+	   private void en(){
+		  
+	   }
+	 /** 
+     * 私钥解密 
+     *  
+     * @param data 
+     * @param privateKey 
+     * @return 
+     * @throws Exception 
+     */  
+    public static String decryptByPrivateKey(String data, String privateKeyStr)  
+            throws Exception {  
+    	byte[] buffer = org.apache.commons.codec.binary.Base64.decodeBase64(privateKeyStr);
+		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+		X509EncodedKeySpec keySpec = new X509EncodedKeySpec(buffer);
+		RSAPrivateKey privateKey = (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
+        Cipher cipher = Cipher.getInstance("RSA");  
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);  
+        //模长  
+        int key_len = privateKey.getModulus().bitLength() / 8;  
+        byte[] bytes = data.getBytes();  
+        byte[] bcd = ASCII_To_BCD(bytes, bytes.length);  
+        System.err.println(bcd.length);  
+        //如果密文长度大于模长则要分组解密  
+        String ming = "";  
+        byte[][] arrays = splitArray(bcd, key_len);  
+        for(byte[] arr : arrays){  
+            ming += new String(cipher.doFinal(arr));  
+        }  
+        return ming;  
+    }  
+    
+    /** 
+     *拆分数组  
+     */  
+    public static byte[][] splitArray(byte[] data,int len){  
+        int x = data.length / len;  
+        int y = data.length % len;  
+        int z = 0;  
+        if(y!=0){  
+            z = 1;  
+        }  
+        byte[][] arrays = new byte[x+z][];  
+        byte[] arr;  
+        for(int i=0; i<x+z; i++){  
+            arr = new byte[len];  
+            if(i==x+z-1 && y!=0){  
+                System.arraycopy(data, i*len, arr, 0, y);  
+            }else{  
+                System.arraycopy(data, i*len, arr, 0, len);  
+            }  
+            arrays[i] = arr;  
+        }  
+        return arrays;  
+    }  
+  
+    /** 
+     * ASCII码转BCD码 
+     *  
+     */  
+    public static byte[] ASCII_To_BCD(byte[] ascii, int asc_len) {  
+        byte[] bcd = new byte[asc_len / 2];  
+        int j = 0;  
+        for (int i = 0; i < (asc_len + 1) / 2; i++) {  
+            bcd[i] = asc_to_bcd(ascii[j++]);  
+            bcd[i] = (byte) (((j >= asc_len) ? 0x00 : asc_to_bcd(ascii[j++])) + (bcd[i] << 4));  
+        }  
+        return bcd;  
+    }  
+    public static byte asc_to_bcd(byte asc) {  
+        byte bcd;  
+  
+        if ((asc >= '0') && (asc <= '9'))  
+            bcd = (byte) (asc - '0');  
+        else if ((asc >= 'A') && (asc <= 'F'))  
+            bcd = (byte) (asc - 'A' + 10);  
+        else if ((asc >= 'a') && (asc <= 'f'))  
+            bcd = (byte) (asc - 'a' + 10);  
+        else  
+            bcd = (byte) (asc - 48);  
+        return bcd;  
+    }  
+    /** 
+     * BCD转字符串 
+     */  
+    public static String bcd2Str(byte[] bytes) {  
+        char temp[] = new char[bytes.length * 2], val;  
+  
+        for (int i = 0; i < bytes.length; i++) {  
+            val = (char) (((bytes[i] & 0xf0) >> 4) & 0x0f);  
+            temp[i * 2] = (char) (val > 9 ? val + 'A' - 10 : val + '0');  
+  
+            val = (char) (bytes[i] & 0x0f);  
+            temp[i * 2 + 1] = (char) (val > 9 ? val + 'A' - 10 : val + '0');  
+        }  
+        return new String(temp);  
+    }  
+    /** 
+     * 拆分字符串 
+     */  
+    public static String[] splitString(String string, int len) {  
+        int x = string.length() / len;  
+        int y = string.length() % len;  
+        int z = 0;  
+        if (y != 0) {  
+            z = 1;  
+        }  
+        String[] strings = new String[x + z];  
+        String str = "";  
+        for (int i=0; i<x+z; i++) {  
+            if (i==x+z-1 && y!=0) {  
+                str = string.substring(i*len, i*len+y);  
+            }else{  
+                str = string.substring(i*len, i*len+len);  
+            }  
+            strings[i] = str;  
+        }  
+        return strings;  
+    } 
 
+    public static void main(String[] args)
+	{
+		System.out.println(encrypt("MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCbIq0lpTe7wD7lBgf81e9rsFsrnYH2opDnNkShv5mFZDX8Vuadm/LtV5cX16ymKAqJhnXwV011FozwYp9DIAZWwHEou3C6r0JdFbJaxCPp7gftf2B/ilf+JWtZQW38ZVED/xie7B2HeOz6bA3B2FCDe7dmnSNjpAVClrlM/SJwbwIDAQAB","nV6x+kFCODZCQUNFLTk3OUQtNDk0Qi05RTU3LUUzOTY1MDQ1QTA1NA=="));
+		System.out.println();
+	}
 	/**
 	 * RSA 加密
 	 *
@@ -43,7 +184,7 @@ public class RSA
 	 * @return
 	 * @throws Exception
 	 */
-	public String encrypt(String publicKeyStr, String plainTextData)
+	public static String encrypt(String publicKeyStr, String plainTextData)
 	{
 
 		try
